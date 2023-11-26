63412526e2b05e2358f7109c6af8297424ea3987
Sanchit Bhatnagar <dev.sanchitb23@gmail.com>
2023-11-26 22:48:16 +0530

    Created Server and PayloadCMS config

---
+++ src/server/config/payload.config.ts
@@ -0,0 +1,42 @@
+import { buildConfig } from "payload/config";
+// @ts-ignore
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+// @ts-ignore
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+// @ts-ignore
+import { slateEditor } from "@payloadcms/richtext-slate";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
+
+export default buildConfig({
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
+  collections: [
+    /* Users, Products, Media, ProductFiles, Orders */
+  ],
+  routes: {
+    admin: "/sell",
+  },
+  admin: {
+    // user: "users",
+    bundler: webpackBundler(),
+    meta: {
+      titleSuffix: "- DigitalHippo",
+      favicon: "/favicon.ico",
+      ogImage: "/thumbnail.jpg",
+    },
+  },
+  rateLimit: {
+    max: 2000,
+  },
+  editor: slateEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URL!,
+  }),
+  typescript: {
+    outputFile: path.resolve(__dirname, "src/server/types/payload-types.ts"),
+  },
+});
